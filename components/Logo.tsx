076ccafe90d554d623586aad239c06fa568aaad8
Eden Gilbert <edengilbertus@proton.me>
2025-09-23 18:18:23 +0300

    Update logo to new PNG version

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,6 @@
 import React, { memo } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { SvgXml } from 'react-native-svg';
-import logoSvg from '../assets/images/logo.svg';
+import { Image } from 'react-native';
 
 interface LogoProps {
   width?: number;
@@ -12,10 +11,10 @@ interface LogoProps {
 function Logo({ width = 160, height = 64, testID }: LogoProps) {
   return (
     <View testID={testID} style={[styles.container, { width, height }]}>
-      <SvgXml 
-        width={width} 
-        height={height} 
-        xml={logoSvg}
+      <Image 
+        source={require('../assets/images/logo.png')}
+        style={{ width: width, height: height }}
+        resizeMode="contain"
       />
     </View>
   );
